c92cb655459aab42119356c91dc57d8271f2f695
Gergely Nagy <algernon@madhouse-project.org>
2012-06-24 05:34:05 +0200

    blog.archive: First approximation of archive generation
    With this patch, we can generate archives for any set of blog
    posts. This also implements a function that groups a whole blog by
    tags, which in turn makes it easy to write per-tag archives.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/madness/core.clj
+++ src/madness/core.clj
@@ -2,7 +2,9 @@
   (:require [madness.io :as io]
             [madness.blog :as blog]
             [madness.blog.index :as blog-index]
-            [madness.blog.post :as blog-post]))
+            [madness.blog.archive :as blog-archive]
+            [madness.blog.post :as blog-post]
+            [madness.utils :as utils]))
 
 (def blog-posts (blog/load-posts))
 
@@ -13,10 +15,24 @@
     (io/write-out-dir fn
                       (apply str (blog-post/blog-post post all-posts)))))
 
+(defn- render-archive
+  [all-posts tag tagged-posts]
+
+  (let [fn (str "." (utils/tag-to-url tag) "index.html")]
+    (io/write-out-dir fn
+                      (apply str (blog-archive/blog-archive all-posts tagged-posts)))))
+
 (defn -main
   []
 
   (io/write-out-dir "index.html"
                     (apply str (blog-index/blog-index blog-posts)))
 
+  (io/write-out-dir "archive.html"
+                    (apply str (blog-archive/blog-archive blog-posts blog-posts)))
+
+  (let [tag-grouped (utils/group-blog-by-tags blog-posts)]
+    (dorun (map #(render-archive blog-posts %1 (get tag-grouped %1))
+                (keys tag-grouped))))
+  
   (dorun (map (partial render-post blog-posts) blog-posts)))
