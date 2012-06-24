ef876b8174eeebc801da5b08b3c587fc87aba5e5
Gergely Nagy <algernon@madhouse-project.org>
2012-06-24 23:02:19 +0200

    asylum: Add the Atom feed template
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- settings.clj
+++ settings.clj
@@ -1,3 +1,6 @@
-{:template {:default "asylum3-main.html"},
+{:template {:default "asylum3-main.html",
+            :atom "asylum3-atom.xml"},
  :recent-posts {:columns 3
-                :rows 2}}
+                :rows 2}
+ :atom {:base-url "http://asylum.madhouse-project.org"
+        :title "Asylum Archives"}}
