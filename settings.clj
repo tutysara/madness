50a96b4086f62d5a3b12192c95849ceac31d836e
Gergely Nagy <algernon@madhouse-project.org>
2012-06-26 20:01:19 +0200

    asylum: Update the templates to reflect the recent change
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- settings.clj
+++ settings.clj
@@ -2,5 +2,6 @@
             :atom "asylum3-atom.xml"},
  :recent-posts {:columns 3
                 :rows 2}
+ :archive-posts {:columns 3}
  :atom {:base-url "http://asylum.madhouse-project.org"
         :title "Asylum Archives"}}
