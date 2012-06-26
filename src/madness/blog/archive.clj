a93ecb27bbc4739ea4490968214ff828faae326e
Gergely Nagy <algernon@madhouse-project.org>
2012-06-26 19:58:47 +0200

    blog.archive: Simplified the archive templating
    The archived posts now reuse the recent-posts template, but remove the
    footer and the summary, keeping only the title.
    
    This also reintroduces the :archive-posts settings.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/madness/blog/archive.clj
+++ src/madness/blog/archive.clj
@@ -8,25 +8,30 @@
             [madness.blog.recent :as blog-recent]
             [clojure.string :as str]))
 
-(h/defsnippet archive-post-item (cfg/template) [:#archive-post]
+(h/defsnippet archive-post-item (cfg/template) [:.recent-post]
   [post]
 
-  [:#archive-post :a] (h/set-attr :href (:url post))
-  [:#archive-post-title] (h/do->
-                          (h/remove-attr :id)
-                          (h/content (:title post)))
-  [:#archive-post-date :span] (h/substitute (utils/date-format (:date post)))
-  [:#archive-post-date] (h/remove-attr :id)
-  [:#archive-post] (h/remove-attr :id))
+  [:.recent-post] (h/remove-class "recent-post")
+  [:h2] (h/add-class "archived")
+  [:h2 :a] (h/do->
+            (h/content " " (:title post))
+            (h/set-attr :href (:url post)))
+  [:.recent-post-footer] nil
+  [:p.summary] nil)
 
-(h/defsnippet archive-posts (cfg/template) [:#archive]
+(h/defsnippet archive-post-row (cfg/template) [:#recents]
   [posts]
 
-  [:#archive] (h/remove-attr :id)
-  [:#archive-post] (h/clone-for [p posts]
-                                (h/do->
-                                 (h/substitute (archive-post-item p))
-                                 (h/after [{:tag :hr}]))))
+  [:#recents] (h/remove-attr :id)
+  [:#recent-posts :.recent-post]
+    (h/clone-for [p posts]
+                 (h/do->
+                  (h/substitute (archive-post-item p))
+                  (h/set-attr :class (str "span" (cfg/archive-posts :span)))))
+  [:#recent-posts] (h/do->
+                    (h/remove-attr :id)
+                    (h/remove-class "visible-desktop")))
+
 
 (h/deftemplate blog-archive (cfg/template)
   [title all-posts blog-posts]
@@ -37,14 +42,18 @@
   [:#full-article-footer] nil
   [:#post-neighbours] nil
   [:#disqus] nil
-  [:#archive] (h/substitute
-               (archive-posts (drop (dec (cfg/recent-posts :total)) blog-posts)))
-  [:#recents]
-    (h/clone-for [rows (utils/blog->table
-                        (cfg/recent-posts :columns)
-                        (cfg/recent-posts :rows) blog-posts)]
-                 (h/do->
-                  (h/substitute (blog-recent/recent-post-row rows true))
-                  (h/before [{:tag :hr}])))
+  [:#recents] (h/clone-for [recent (utils/blog->table
+                                    (cfg/recent-posts :columns)
+                                    (cfg/recent-posts :rows) blog-posts)]
+                           (h/do->
+                            (h/substitute (blog-recent/recent-post-row recent true))
+                            (h/before [{:tag :hr}])))
+  [:#archive] (h/clone-for [archive (utils/blog->table
+                                     (cfg/archive-posts :columns)
+                                     (cfg/archive-posts :rows)
+                                     (drop (dec (cfg/recent-posts :total)) blog-posts))]
+                           (h/do->
+                            (h/substitute (archive-post-row archive))
+                            (h/before [{:tag :hr}])))
   [:#nav-recent-posts :ul :li] (blog-nav/recent-posts all-posts)
   [:#nav-tags :ul :li] (blog-nav/all-tags all-posts))
