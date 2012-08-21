d83575c57081ea42a43b28cb114b870c0bb49a4c
Gergely Nagy <algernon@madhouse-project.org>
2012-08-21 12:17:40 +0200

    m.io: Create parent directories before spitting out a file
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/madness/io.clj
+++ src/madness/io.clj
@@ -7,7 +7,8 @@
               :url "http://www.gnu.org/licenses/gpl.txt"}}
 
   (:require [madness.config :as cfg]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [fs.core :as fs]))
 
 (defn write-out-dir
   "Given a filename, write a string into it, creating the file and the
@@ -18,4 +19,5 @@
 
   (let [fn (str/join "/" [(cfg/dirs :output) file])]
     (println "Writing " fn "...")
+    (fs/mkdirs (fs/parent fn))
     (spit fn str :encoding "UTF-8")))
